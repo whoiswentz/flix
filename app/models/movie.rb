5236dfd9abe98d5a880a215caab4cc3c7ecb10f0
Vinicios Wentz <vinicios@wentz.io>
2021-02-02 22:58:29 -0300

    feature: add links and paths to the application

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-    def flop?
-        total_gross.blank? || total_gross < 255_000_000
-    end
+  def flop?
+    total_gross.blank? || total_gross < 255_000_000
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
+  root "movies#index"
+
   get 'movies', to: 'movies#index'
+  get 'movies/:id', to: 'movies#show', as: "movie"
 end
