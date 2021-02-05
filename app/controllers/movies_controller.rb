72fdf3ffcafd604d8afa4e8e1c2a314fe32016dd
Vinicios Wentz <vinicios@wentz.io>
2021-02-04 22:33:47 -0300

    feature: ensure that a movie can be updated

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,19 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    redirect_to @movie if @movie.update(update_params)
+  end
+
+  private
+  def update_params
+    params.require(:movie).permit(:title, :description, :total_gross, :released_at)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   get 'movies', to: 'movies#index'
   get 'movies/:id', to: 'movies#show', as: "movie"
+  get "movies/:id/edit", to: "movies#edit", as: "movie_edit"
+  patch "movies/:id", to: "movies#update"
 end
