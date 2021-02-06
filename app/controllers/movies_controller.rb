f5a3fe36edcfc9397e9e29795c67a2334b6af6d0
Vinicios Wentz <vinicios@wentz.io>
2021-02-05 21:56:22 -0300

    feature: movie creation

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,12 +13,20 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    redirect_to @movie if @movie.update(update_params)
+    redirect_to @movie if @movie.update(movie_params)
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    redirect_to @movie if @movie.save
   end
 
   private
-  def update_params
+  def movie_params
     params.require(:movie).permit(:title, :description, :total_gross, :released_at)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root "movies#index"
+  root 'movies#index'
 
-  get 'movies', to: 'movies#index'
-  get 'movies/:id', to: 'movies#show', as: "movie"
-  get "movies/:id/edit", to: "movies#edit", as: "movie_edit"
-  patch "movies/:id", to: "movies#update"
+  resources :movies
 end
