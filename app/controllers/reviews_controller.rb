cb88ce6467fab14962ecdb213f941527d60dcf10
Vinicios Wentz <vinicios@wentz.io>
2021-02-07 19:46:58 -0300

    feature: movie review registration form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,31 @@
 class ReviewsController < ApplicationController
+  before_action :set_review
+
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
+  end
+
+  def new
+    @review = @movie.reviews.new
+  end
+
+  def create
+    @review = @movie.reviews.new(review_params)
+
+    if @review.save
+      redirect_to movie_reviews_url, notice: 'Thanks for the review'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:name, :comment, :stars)
+  end
+
+  def set_review
+    @movie = Movie.find(params[:movie_id])
   end
 end
