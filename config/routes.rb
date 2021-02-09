7d797b680b3f36d5dde573a1f279ace25dfe8891
Vinicios Wentz <vinicios@wentz.io>
2021-02-08 22:30:52 -0300

    feature: ensure that UserController can index, show and create new users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
   root 'movies#index'
 
   resources :movies do
     resources :reviews
   end
+
+  resources :users
+  get 'signup', to: 'users#new'
 end
