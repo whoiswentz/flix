6ad618b6b7d340477ea8f995198cc5bafe8b3c42
Vinicios Wentz <vinicios@wentz.io>
2021-02-11 22:35:53 -0300

    refactor: removed the rescue clause in the application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
   def current_user
-    begin
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    rescue ActiveRecord::RecordNotFound
-      nil
-    end
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
