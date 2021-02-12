c6b310079114c0ab4cd663f385b7c3fedc872da1
Vinicios Wentz <vinicios@wentz.io>
2021-02-11 21:43:59 -0300

    feature: ensure that users can auto signin when signup and see they account settings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user, notice: "Thanks for signing up"
+      session[:user_id] = @user.id
+      redirect_to @user, notice: 'Thanks for signing up'
     else
       render :new
     end
