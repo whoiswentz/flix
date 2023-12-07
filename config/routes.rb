fadc41706d728016d3fe87add24c11fe78282598
Vinicios Wentz <vinicios@wentz.io>
2023-12-07 20:05:32 -0300

    Routes: Linking Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Defines the root path route ("/")
+  root "movies#index"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-
   get "movies" => "movies#index"
-  get "movies/:id" => "movies#show"
+  get "movies/:id" => "movies#show", as: :movie
 end
