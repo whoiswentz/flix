bd09f8e6e350b4ecc7513711b192dc513de193ea
Vinicios Wentz <vinicios@wentz.io>
2023-12-08 11:49:12 -0300

    Forms: Editing Records

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,28 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update]
+
   def index
     @movies = Movie.all
   end
 
-  def show
+  def show; end
+
+  def edit; end
+
+  def update
+    @movie.update(update_params)
+    redirect_to @movie
+  end
+
+  private
+
+  def update_params
+    params
+      .require(:movie)
+      .permit(:title, :rating, :description, :released_on, :total_gross)
+  end
+
+  def set_movie
     @movie = Movie.find(params[:id])
   end
 end
