380796b96610143ed7511e38095f5edf85899024
Vinicios Wentz <vinicios@wentz.io>
2023-12-14 14:33:54 -0300

    Model Validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,14 @@
 class Movie < ApplicationRecord
-  validates :title, presence: true
-  validates :description, presence: true
+  RATINGS = %w(G PG PG-13 R NC-17)
+
+  validates :title, :released_on, :duration, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+  validates :image_file_name, format: {
+    with: /\w+\.(jpg|png)\z/i,
+    message: "must be a JPG or PNG image"
+  }
+  validates :rating, inclusion: { in: RATINGS }
 
   scope :released_on_order, -> (direction) { order("released_on #{direction}") }
   scope :released, -> { released_on_order("desc").where("released_on < ?", Time.now) }
