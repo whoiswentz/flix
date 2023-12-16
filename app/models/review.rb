15ad1aaf98952210c8c95e5cd8c0a1e9ab282bed
Vinicios Wentz <vinicios@wentz.io>
2023-12-15 23:11:57 -0300

    One-to-Many: has_many

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
+  STARS = [1, 2, 3, 4, 5]
   belongs_to :movie
+
+  validates :name, presence: true
+  validates :comment, length: { minimum: 4 }
+  validates :stars, inclusion: { in: STARS, message: "must be between 1 and 5" }
 end
