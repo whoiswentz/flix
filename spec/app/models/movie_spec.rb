a8d9411d65c8d47a4835044fe1f243975b524855
Vinicios Wentz <vinicios@wentz.io>
2023-12-15 23:22:01 -0300

    Movie model tests

--- spec/app/models/movie_spec.rb
+++ spec/app/models/movie_spec.rb
@@ -2,5 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
   it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_presence_of(:released_on) }
+  it { is_expected.to validate_length_of(:description).is_at_least(25) }
+  it { is_expected.to validate_numericality_of(:total_gross).is_greater_than_or_equal_to(0) }
+
+  describe 'image_file_name' do
+    it { should allow_value('image.jpg').for(:image_file_name) }
+    it { should allow_value('image.png').for(:image_file_name) }
+    it { should_not allow_value('image.gif').for(:image_file_name).with_message('must be a JPG or PNG image') }
+    it { should_not allow_value('image.pdf').for(:image_file_name).with_message('must be a JPG or PNG image') }
+    it { should_not allow_value('image').for(:image_file_name).with_message('must be a JPG or PNG image') }
+  end
+
+  it { should validate_inclusion_of(:rating).in_array(Movie::RATINGS) }
 end
--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :movie do
-    name { Faker::Movie.title }
+    title { Faker::Movie.title }
     description { Faker::Movie.quote }
     released_on { "2023-09-21 13:27:40" }
+    image_file_name { "image.png" }
   end
 end
