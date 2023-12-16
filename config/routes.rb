d3309b92c3a69a06a78038e95430357f3eb018a3
Vinicios Wentz <vinicios@wentz.io>
2023-12-15 23:51:27 -0300

    One-to-Many: Nested Resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :reviews
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -9,5 +8,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :movies
+  resources :movies do
+    resources :reviews
+  end
 end
