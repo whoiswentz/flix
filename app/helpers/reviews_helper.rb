efe83371d09daedfc0abe76aae23a702ce39dfa1
Vinicios Wentz <vinicios@wentz.io>
2023-12-18 18:22:57 -0300

    One-to-Many: Logic

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,9 @@
 module ReviewsHelper
+  def average_stars(movie)
+    if movie.average_stars.zero?
+      content_tag(:strong, "No reviews")
+    else
+      pluralize(number_with_precision(movie.average_stars, precision: 1) , "star")
+    end
+  end
 end
